3370fd05dab16de7b5d6255112aef71b303adcb9
Priya Sharma <priya@smartsolution.dev>
2026-01-28 11:43:22 +0200

    swap values of two variable

---
+++ destructuring.js
@@ -0,0 +1,15 @@
+// destructuring = extracting values from arrays or objects into distinct variables.
+// [] = array destructuring
+// {} = object destructuring
+
+
+// swap the values of two variables
+let a = 5;
+let b = 11;
+[a,b] = [b,a];
+console.log(a); //11
+console.log(b); //5
+
+console.log("-".repeat(50));
+
+// extract values from an array
